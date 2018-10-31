262438ea0aaa95933ff82ae9a7ab227751ea893b
uvaaland <uvaaland@princeton.edu>
2018-10-31 11:18:13 -0400

    Initial commit

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH -N 2
+#SBATCH --ntasks-per-node=1
+#SBATCH -t 00:30:00
+#SBATCH --reservation=bootcamp
+
+module load anaconda3
+module load intel-mpi/gcc/2018.3/64
+conda activate mpi4py
+
+srun python osu_bw.py
